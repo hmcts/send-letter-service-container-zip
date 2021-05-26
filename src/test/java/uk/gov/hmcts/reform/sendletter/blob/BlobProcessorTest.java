c7f224435bb6671c38e2e183a5d0a8b97d6254da
rishisharma <rishi.sharma@hmcts.net>
2021-05-26 11:26:54 +0100

    DTSBPS-430: initial commit

--- src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
