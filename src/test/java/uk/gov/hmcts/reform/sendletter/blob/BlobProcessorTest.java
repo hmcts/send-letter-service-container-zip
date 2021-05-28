a5dcdf6a2fcd9ae0e3a051f2a7ba3a7d0ad8120d
SomeshRA <somesh.acharya1@hmcts.net>
2021-05-28 18:11:45 +0100

    DTSBPS-431: Encrypting the zip file from process folder

--- src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
+++ src/test/java/uk/gov/hmcts/reform/sendletter/blob/BlobProcessorTest.java
@@ -10,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.sendletter.blob.component.BlobEncryption;
 import uk.gov.hmcts.reform.sendletter.blob.component.BlobReader;
 import uk.gov.hmcts.reform.sendletter.blob.component.BlobUpload;
+import uk.gov.hmcts.reform.sendletter.exceptions.LeaseIdNotPresentException;
 import uk.gov.hmcts.reform.sendletter.model.BlobInfo;
 import uk.gov.hmcts.reform.sendletter.model.EncryptedInfo;
 
@@ -17,6 +18,7 @@ import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.never;
@@ -80,5 +82,19 @@ class BlobProcessorTest {
         verify(blobClient, never()).deleteWithResponse(any(), any(), any(), any());
     }
 
+    @Test
+    void should_throw_lease_id_not_present_expection_when_leaseId_absent() {
+        var encryptedInfo = new EncryptedInfo(
+                "encrypted.pgp",
+                "Encrypted Data".getBytes(StandardCharsets.UTF_8));
 
+        BlobInfo blobInfo = new BlobInfo(blobClient);
+        given(blobReader.retrieveBlobToProcess()).willReturn(Optional.of(blobInfo));
+        given(blobEncryption.process(blobClient)).willReturn(encryptedInfo);
+
+        assertThatThrownBy(() -> blobProcessor.read())
+                .isInstanceOf(LeaseIdNotPresentException.class)
+                .hasMessage("Lease not present");
+
+    }
 }
