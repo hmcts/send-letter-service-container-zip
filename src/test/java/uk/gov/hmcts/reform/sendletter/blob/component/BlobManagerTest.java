ccce547d02c89e5ff21f1b68d49ad580c77c9421
SomeshRA <somesh.acharya1@hmcts.net>
2021-05-28 17:50:41 +0100

    DTSBPS-431: Encrypting the zip file from process folder

---
+++ src/test/java/uk/gov/hmcts/reform/sendletter/blob/component/BlobManagerTest.java
@@ -0,0 +1,82 @@
+package uk.gov.hmcts.reform.sendletter.blob.component;
+
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.common.StorageSharedKeyCredential;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import uk.gov.hmcts.reform.sendletter.config.AccessTokenProperties;
+import uk.gov.hmcts.reform.sendletter.services.SasTokenGeneratorService;
+
+import java.util.function.BiFunction;
+
+import static java.util.List.of;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class BlobManagerTest {
+    @Mock
+    private BlobServiceClient blobServiceClient;
+
+    private String sasToken;
+    private BlobManager blobManager;
+    private AccessTokenProperties accessTokenProperties;
+
+    @BeforeEach
+    void setUp() {
+
+        blobManager = new BlobManager(blobServiceClient);
+
+        StorageSharedKeyCredential storageCredentials =
+                new StorageSharedKeyCredential("testAccountName", "dGVzdGtleQ==");
+
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .credential(storageCredentials)
+                .endpoint("http://test.account")
+                .buildClient();
+
+        createAccessTokenConfig();
+
+        SasTokenGeneratorService tokenGeneratorService =
+                new SasTokenGeneratorService(blobServiceClient, accessTokenProperties);
+        sasToken = tokenGeneratorService.generateSasToken("zipped");
+    }
+
+    @Test
+    void retrieves_container_from_client() {
+        BlobContainerClient expectedContainer = mock(BlobContainerClient.class);
+        String containerName = "container-name";
+
+        given(blobServiceClient.getBlobContainerClient(any())).willReturn(expectedContainer);
+        BlobContainerClient actualContainer = blobManager.getContainerClient(containerName);
+
+        assertThat(actualContainer).isSameAs(expectedContainer);
+        verify(blobServiceClient).getBlobContainerClient(containerName);
+    }
+
+    private void createAccessTokenConfig() {
+        BiFunction<String, String, AccessTokenProperties.TokenConfig> tokenFunction = (type, container) -> {
+            AccessTokenProperties.TokenConfig tokenConfig = new AccessTokenProperties.TokenConfig();
+            tokenConfig.setValidity(300);
+            tokenConfig.setContainerType(type);
+            tokenConfig.setContainerName(container);
+            return tokenConfig;
+        };
+        accessTokenProperties = new AccessTokenProperties();
+        accessTokenProperties.setServiceConfig(
+                of(
+                        tokenFunction.apply("source", "zipped"),
+                        tokenFunction.apply("destination", "encrypted")
+
+                )
+        );
+    }
+}
