3481809bd232f1dd37f5303bfca45e9d5d8e162d
rishisharma <rishi.sharma@hmcts.net>
2021-06-22 12:48:07 +0100

    DTSBPS-642: logging message in app-insight

--- src/main/java/uk/gov/hmcts/reform/sendletter/Application.java
+++ src/main/java/uk/gov/hmcts/reform/sendletter/Application.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.sendletter;
 
+import com.microsoft.applicationinsights.TelemetryClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,8 @@ public class Application implements CommandLineRunner {
 
     @Autowired
     private BlobProcessor retrieve;
+    @Autowired
+    private TelemetryClient telemetryClient;
 
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
@@ -25,11 +28,21 @@ public class Application implements CommandLineRunner {
     @Override
     public void run(String... args) {
         try {
+            telemetryClient.trackEvent("send letter zipped KEDA container invoked");
             LOGGER.info("send letter zipped KEDA container invoked");
             retrieve.read();
             LOGGER.info("send letter zipped KEDA container finished processing");
         } catch (Exception e) {
             LOGGER.info("Exception occurred while zipped KEDA container invoked", e);
+        } finally {
+            // Initiate flush and give it some time to finish.
+            telemetryClient.flush();
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException ie) {
+                LOGGER.error("Exception in thread sleep", ie);
+                Thread.currentThread().interrupt();
+            }
         }
     }
 }
